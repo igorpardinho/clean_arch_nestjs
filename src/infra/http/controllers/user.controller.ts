ddf98adb982305bc09c9126af7a93f60456b4cca
Igor Pardinho <pardinhoigor@gmail.com>
2025-06-03 13:12:13 -0300

    cria o post do create para user

--- src/infra/http/controllers/user.controller.ts
+++ src/infra/http/controllers/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { UserResponseDto } from './../../../application/dto/user-response.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserService } from '../../../application/services/create-user.service';
 import { FindAllUserService } from '../../../application/services/findAll-user.service';
-import { UserResponseDto } from '../../../application/dto/user-response.dto';
+import { CreateUserDto } from '../../../application/dto/create-user.dto';
 
 @Controller('users')
 export class UserController {
@@ -11,9 +12,14 @@ export class UserController {
   ) {}
 
   @Get()
-  async findAll() {
+  async findAll(): Promise<UserResponseDto[]> {
     const users = await this.findAllUserService.execute();
 
     return users.map((user) => UserResponseDto.fromDomain(user));
+  }
+
+  @Post()
+  async create(@Body() dto: CreateUserDto): Promise<void> {
+    return await this.createUserService.execute(dto);
   }
 }
